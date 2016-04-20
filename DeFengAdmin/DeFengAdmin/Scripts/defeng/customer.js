784e75124bf7e3330824ada1a17a8a6994fa3a5a
hello-jim <842736598@qq.com>
2016-04-20 10:45:10 +0800

    ,,

--- DeFengAdmin/DeFengAdmin/Scripts/defeng/customer.js
+++ DeFengAdmin/DeFengAdmin/Scripts/defeng/customer.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function () {
     $("#main-menu li").removeClass("active");
     $(".customer-menu").addClass("opened active");
+    InitHouseAdd();
 
 });
 
@@ -57,6 +58,85 @@ function GetCustomerStatusObj() {
     return house;
 }
 
+function InitHouseAdd() {
+    $(".add-customer").on("click", function () {
+        ShowCustomerPanel("addCustomer");
+        InitProvince("#provinceSelect", "#citySelect", "#districtSelect", "#areaSelect", "", true);
+        InitResidentialDistrict("#residentialDistrictSelect", "", true);
+        InitHouseType("#houseTypeSelect", "", true);
+        InitHouseUseType("#houseTypeSelect", "", true);
+        InitOrientation("#orientationSelect", "", true);
+        InitHousingLetter("#housingLetterSelect", "", true);
+        InitHouseQuality("#houseQualitySelect", "", true);
+        InitTransactionType("#transactionTypeSelect", "", true);
+        InitHouseStatus("#houseStatusSelect", "", true);
+        InitTaxPayType("#taxPayTypeSelect", "", true);
+        InitEntrustType("#entrustTypeSelect", "", true);
+        InitSource("#sourceSelect", "", true);
+        InitCurrent("#currentSelect", "", true);
+        InitPropertyOwn("#propertyOwnSelect", "", true);
+        InitDecorationType("#decorationTypeSelect", "", true);
+        InitHouseDocumentType("#houseDocumentTypeSelect", "", true);
+        InitCommissionPayType("#commissionPayTypeSelect", "", true);
+        InitSupporting("#supportingSelect", "", true);
+        InitHousePayType("#housePayTypeSelect", "", true);
+        InitFurniture("#furnitureSelect", "", true);
+        InitAppliance("#applianceSelect", "", true);
+        InitLookHouseType("#lookHouseTypeSelect", "", true);
+        $("#addHouse").on("click", function () {
+            var house = GetHouseObj();
+            var houseJson = JSON.stringify(house);
+            $.post("/House/AddHouse",
+                {
+                    house: houseJson
+                }, function (data) {
+                    if (data == "1") {
+                        alert("success");
+                    }
+                });
+        });
+    });
+}
 
+function ShowCustomerPanel(action) {
+    $(".customer-panel *").unbind("click");
 
+    $(".customer-panel-close").on("click", function () {
+        $(".customer-panel").hide();
+    });
+    $('.form_datetime').datetimepicker({
+        format: 'yyyy-mm-dd',
+        language: 'zh-CN',
+        // weekStart: 1,
+        todayBtn: 1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        minView: 2,
+        forceParse: 0
+    });
+    $(".customer-panel").show();
+    var scrollTop = $(document).scrollTop();
+    $(".customer-panel").css("top", scrollTop + 400 + "px");
+    if (action == "editHouse") {
+        $("#fileUp").show();
+        $(".follow-record-panel-show").show();
+        var recordBtnIsShow = $(".follow-record-show").css("display") == "inline-block";
+        if (recordBtnIsShow) {
+            $(".owner-show").hide();
+        }
+        $("#addCustomer").hide();
+        var houseID = $("#houseID").val();
+    } else {
+        $("#fileUp").hide();
+        $(".follow-record-panel-show").hide();
+        $(".owner-show").hide();
+        $("#addCustomer").show();
+    }
+    $("#" + action + "").show();
+    $(".follow-record-panel-show").on("click", function () {
+        $(".follow-record-panel").css("top", scrollTop + 500 + "px");
+        ShowFollowRecordPanel();
+    });
+}
 
