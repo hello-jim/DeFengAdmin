6fcaae2823113c4a33807f96d2c247bdb2e82b7d
LIZIDEEP <540948293@qq.com>
2016-04-29 18:26:20 +0800

    。。

--- DeFengAdmin/DeFengAdmin/Controllers/HomeController.cs
+++ DeFengAdmin/DeFengAdmin/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DeFeng.Model;
+using DeFeng.BLL;
 
 namespace DeFengAdmin.Controllers
 {
@@ -26,5 +28,31 @@ namespace DeFengAdmin.Controllers
 
             return View();
         }
+
+       public ActionResult Register()
+        {
+            var request = Request;
+            var name = request["name"] != null ? Convert.ToString(request["name"]) : "";
+            var password = request["passWord"] != null ? Convert.ToString(request["passWord"]) : "";
+            var idCard = request["idCard"] != null ? Convert.ToString(request["idCard"]) : "";
+            var phone = request["phone"] != null ? Convert.ToString(request["phone"]) : "";
+            Staff staff = new Staff()
+            {
+                Name = name,
+                Password = password,
+                IdCard = idCard,
+                Phone = phone        
+            };
+            Staff_BLL bll = new Staff_BLL();
+            bll.Register(staff);
+  
+            return View();
+        }
+
+        public ActionResult Information()
+        {
+            return View();
+        }
+
     }
 }
